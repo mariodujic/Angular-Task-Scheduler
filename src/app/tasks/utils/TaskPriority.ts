fe0169e052c8d40c04b629d62f09782e3f75983b
mario dujic <mario.dujic@gmail.com>
2019-08-04 13:28:00 +0200

    refactored TaskPriority class

--- src/app/tasks/utils/TaskPriority.ts
+++ src/app/tasks/utils/TaskPriority.ts
@@ -1,10 +1,15 @@
 import {TaskStatus} from './TaskStatus';
 import {TimeStamp} from './TimeStamp';
 
+// TODO Give user possibility to change these dynamically in settings
+const STATUS_LOW_SECONDS = 5;
+const STATUS_NORMAL_SECONDS = 8;
+
 export class TaskPriority {
+
   // Priority depends on time passed since task has been posted.
   public static getPriority(taskDate: Date): string {
-    return TimeStamp.getSecondsPassed(taskDate) < 5 ? TaskStatus.LOW :
-      TimeStamp.getSecondsPassed(taskDate) < 8 ? TaskStatus.NORMAL : TaskStatus.URGENT;
+    return TimeStamp.getSecondsPassed(taskDate) < STATUS_LOW_SECONDS ? TaskStatus.LOW :
+      TimeStamp.getSecondsPassed(taskDate) < STATUS_NORMAL_SECONDS ? TaskStatus.NORMAL : TaskStatus.URGENT;
   }
 }
